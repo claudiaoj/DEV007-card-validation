c2511231c25b4b47954162274362fac17db4d3a6
claudiaoj <clau@MacBook-Air-de-Claudia.local>
2023-04-24 15:31:23 -0400

    primer commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import validator from './validator.js';
 
-//console.log(validator);
+const numeroDeTarjeta = document.getElementById("pago")
+numeroDeTarjeta.addEventListener("click", (e) => obtenerNumeroTarjeta(e)) //Al hacer click, se llama a la funcion obtenerNumeroTarjeta.
 
-const numeroTarjeta = document.getElementById("pago");
-numeroTarjeta.addEventListener("click", obtenerNumeroTarjeta)
+function obtenerNumeroTarjeta(e) {
+  e.preventDefault();                                                   //Previene la accion por defecto del evento click, que es recargar la página. 
 
-function obtenerNumeroTarjeta() {
-    let numeroIngresado = document.getElementById("numTarjeta").value;
-    
-    //console.log(numeroIngresado)
+  const numeroIngresado = document.getElementById("numTarjeta").value;    //Obtener el valor del input donde el usuario ingresa el número de tarjeta.
+  
+  if (numeroIngresado.trim() === '') {                       //Validar que el input no este vacio. Si esta vacio, muestra la alerta y termina con un return. 
+    alert("Ingrese un número de tarjeta de crédito");   //Trim() se usa para eliminar los espacios en blanco del número ingresado y 
+    return;                                                   //Luego compara el resultado con una cadena vacía ('') utilizando el operador de igualdad estricta (===) 
 
-    let esValido = validator.isValid(numeroIngresado);
-    console.log(esValido);
+  }
+  
+  if (validator.isValid(numeroIngresado)) {                // Validar si el número de tarjeta es válido usando la función isValid del módulo validator.
+    alert("La tarjeta de crédito es válida");
+  } else {
+    alert("La tarjeta de crédito no es válida");
+    return;
+  }
+  
+  const numFinal = validator.maskify(numeroIngresado, { mostrarUltimosNumeros: 4 });   //Enmascarar el número de tarjeta utilizando la función maskify del módulo validator y muestra solo los últimos 4 dígitos
+  alert("Número de tarjeta: " + numFinal);
+  
 }
 
 
--- src/validator.js
+++ src/validator.js
@@ -1,7 +1,52 @@
-const validator = {
-  // ... isValid...
+const validator = {          //objeto, contiene 2 metodos isvalid e maskify.
+
+  isValid: function (creditCardNumber) { // Función que verifica si una tarjeta de crédito es válida o no. Recibe como parámetro(crediCardNumber) el número de la tarjeta.
+
+    // algoritmo de Luhn
+    let sum = 0;                                                  //var sum en 0, para almacenar la suma de los digitos de la tarjeta.
+    
+    for (let i = creditCardNumber.length - 2; i >= 0; i = i - 2) {   // El primer ciclo, recorre los dígitos de la tarjeta de crédito en posiciones pares, de derecha a izquierda.
+      const dobleNum = parseInt(creditCardNumber[i]) * 2;         // Multiplica el dígito actual por dos.
+      
+      if (dobleNum >= 10) {
+        sum += dobleNum - 9;
+      } else {
+        sum += dobleNum;
+      }
+    }
+
+    for (let i = creditCardNumber.length - 1; i >= 0; i = i - 2) { // El segundo bucle recorre los dígitos de la tarjeta de crédito en posiciones impares, de derecha a izquierda.
+      sum += parseInt(creditCardNumber[i]);                     // Agrega el dígito actual a la variable 'sum'.
+    }
+
+    if (sum % 10 === 0) {                        
+      return true;           // la tarjeta es válida
+    } else
+      return false;            // la tarjeta no es válida         
+  },
+    
+
+  maskify: function (creditCardNumber) {
+    if (creditCardNumber.length <= 4) {
+      return creditCardNumber
+    }
+    const ultimosCuatroNum = creditCardNumber.slice(-4);
+    const numeroEnmascarado = "#".repeat(creditCardNumber.length - 4) + ultimosCuatroNum;
+    return numeroEnmascarado;
+  }
 
 };
 
 export default validator;
+
+
+
+
+
+
+
+
+
+
+
 
