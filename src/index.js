bd167593a0f9a54c80626d23ce8c36f369b3679e
claudiaoj <clau@MacBook-Air-de-Claudia.local>
2023-04-27 22:13:49 -0400

    Final

--- src/index.js
+++ src/index.js
@@ -1,27 +1,33 @@
 import validator from './validator.js';
 
+//Se obtiene al id pago y al hacer click, se llama a la funcion obtenerNumeroTarjeta.
 const numeroDeTarjeta = document.getElementById("pago")
-numeroDeTarjeta.addEventListener("click", (e) => obtenerNumeroTarjeta(e)) //Al hacer click, se llama a la funcion obtenerNumeroTarjeta.
+numeroDeTarjeta.addEventListener("click", (e) => obtenerNumeroTarjeta(e)) 
 
 function obtenerNumeroTarjeta(e) {
-  e.preventDefault();                                                   //Previene la accion por defecto del evento click, que es recargar la página. 
-
-  const numeroIngresado = document.getElementById("numTarjeta").value;    //Obtener el valor del input donde el usuario ingresa el número de tarjeta.
+  //Previene la accion por defecto del evento click.
+  e.preventDefault();   
   
-  if (numeroIngresado.trim() === '') {                       //Validar que el input no este vacio. 
-    alert("Ingrese un número de tarjeta de crédito");        //Trim() elimina espacios en blanco y compara el resultado con una cadena vacía ('') usando (===).
+  //Obtener el valor del input donde el usuario ingresa el número de tarjeta.
+  const numeroIngresado = document.getElementById("numTarjeta").value;    
+  
+  //Trim() elimina espacios en blanco y compara el resultado con una cadena vacía ('').
+  if (numeroIngresado.trim() === '') {                     
+    alert("Ingrese un número de tarjeta de crédito");       
     return;                                                     
 
   }
   
-  if (validator.isValid(numeroIngresado)) {                // Validar si el número de tarjeta es válido usando la función isValid del módulo validator.
+  // Validar si el número de tarjeta es válido usando la función isValid de validator.
+  if (validator.isValid(numeroIngresado)) {                
     alert("La tarjeta de crédito es válida");
   } else {
     alert("La tarjeta de crédito no es válida");
     return;
   }
   
-  const numFinal = validator.maskify(numeroIngresado, { mostrarUltimosNumeros: 4 });   //Enmascarar el número de tarjeta utilizando la función maskify del módulo validator y muestra solo los últimos 4 dígitos
+  //Enmascarar el número de tarjeta utilizando la función maskify y muestra solo los últimos 4 dígitos.
+  const numFinal = validator.maskify(numeroIngresado, { mostrarUltimosNumeros: 4 });   
   alert("Número de tarjeta: " + numFinal);
 
 }
--- src/validator.js
+++ src/validator.js
@@ -1,37 +1,50 @@
-const validator = {          //objeto, contiene 2 metodos isvalid e maskify.
+const validator = {          //objeto: contiene 2 metodos isvalid e maskify.
 
-  isValid: function (creditCardNumber) { // Función que verifica si una tarjeta de crédito es válida o no. Recibe como parámetro(crediCardNumber) el número de la tarjeta.
+  // Función que verifica si una tarjeta de crédito es válida o no. Recibe como parámetro(crediCardNumber).
+  isValid: function (creditCardNumber) { 
 
     // algoritmo de Luhn
-    let sum = 0;                                                  //var sum en 0, para almacenar la suma de los digitos de la tarjeta.
+    //var sum en 0, para almacenar la suma de los digitos de la tarjeta.
+    let sum = 0;                                                
     
-    for (let i = creditCardNumber.length - 2; i >= 0; i = i - 2) {   // El primer ciclo, recorre los dígitos de la tarjeta de crédito en posiciones pares, de derecha a izquierda.
-      const dobleNum = parseInt(creditCardNumber[i]) * 2;         // Multiplica el dígito actual por dos.
-      
+    // El primer ciclo, recorre los dígitos de la tarjeta de crédito en posiciones pares, de derecha a izquierda.
+    for (let i = creditCardNumber.length - 2; i >= 0; i = i - 2) {   
+
+      // Multiplica el dígito actual por dos.
+      const dobleNum = parseInt(creditCardNumber[i]) * 2;       
+
+      //Condicion.
       if (dobleNum >= 10) {
         sum += dobleNum - 9;
       } else {
         sum += dobleNum;
       }
     }
+    // El segundo bucle recorre los dígitos de la tarjeta de crédito en posiciones impares, de derecha a izquierda.
+    for (let i = creditCardNumber.length - 1; i >= 0; i = i - 2) { 
 
-    for (let i = creditCardNumber.length - 1; i >= 0; i = i - 2) { // El segundo bucle recorre los dígitos de la tarjeta de crédito en posiciones impares, de derecha a izquierda.
-      sum += parseInt(creditCardNumber[i]);                     // Agrega el dígito actual a la variable 'sum'.
+      // Agrega el dígito actual a la variable 'sum'. += oper. asignacion
+      sum += parseInt(creditCardNumber[i]);                     
     }
 
-    if (sum % 10 === 0) {                        
+    //Condición.
+    if (sum % 10 === 0) {     
       return true;           // la tarjeta es válida
     } else
-      return false;            // la tarjeta no es válida         
+      return false;          // la tarjeta no es válida         
   },
-    
+  
 
+  // Función que enmascara los números de la TC.
   maskify: function (creditCardNumber) {
     if (creditCardNumber.length <= 4) {
       return creditCardNumber
     }
+    //Se obtiene los últimos 4 numeros de la TC y se almacena en la variable.
     const ultimosCuatroNum = creditCardNumber.slice(-4);
-    const numeroEnmascarado = "#".repeat(creditCardNumber.length - 4) + ultimosCuatroNum;
+  
+    // El "#" se repite con una longitud igual a la cantidad de caracteres del número de tarjeta menos los últimos 4.
+    const numeroEnmascarado = "#".repeat(creditCardNumber.length - 4) + ultimosCuatroNum; 
     return numeroEnmascarado;
   }
 
